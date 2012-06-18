2af1d3177e0edcaef8aa786e5537918a417174e6
trinea1989@gmail.com <trinea1989@gmail.com@0036957c-3a92-a46f-185b-4da59575e3b1>
2012-06-18 03:04:35 +0000

    http://code.google.com/p/trinea-android-common/这个工程对应的demo
    DemoListDemo第一个activity，列举出所有的demo，并可以点击进入体验响应的demo
    
    DropDownToRefreshListViewDemo为DropDownToRefreshListView的demo
    
    CompoundDrawablesTextViewDemo为CompoundDrawablesTextView的demo

---
+++ src/com/trinea/common/activity/DemoListDemo.java
@@ -0,0 +1,51 @@
+package com.trinea.common.activity;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+
+import com.trinea.common.R;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+/**
+ * demo list 列表
+ * 
+ * @author Trinea 2012-6-17 下午06:14:57
+ */
+public class DemoListDemo extends Activity {
+
+    private String[] mStrings = {"Go To DropDownToRefreshListView Demo", "Go To CompoundDrawablesTextView Demo"};
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.demo_list);
+
+        LinkedList<String> mListItems = new LinkedList<String>();
+        mListItems.addAll(Arrays.asList(mStrings));
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mListItems);
+
+        ListView demoListView = (ListView)findViewById(R.id.demoListView);
+        demoListView.setAdapter(adapter);
+        demoListView.setOnItemClickListener(new OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if (position == 0) {
+                    Intent intent = new Intent(DemoListDemo.this, DropDownToRefreshListViewDemo.class);
+                    startActivity(intent);
+                } else if (position == 1) {
+                    Intent intent = new Intent(DemoListDemo.this, CompoundDrawablesTextViewDemo.class);
+                    startActivity(intent);
+                }
+            }
+        });
+    }
+}
